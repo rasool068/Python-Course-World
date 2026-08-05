11669cc8de57dbfd73b7c169fd657435e0fb0718
Rasool Dudekula <rasooldudekula268@gmail.com>
2026-08-05 17:48:09 +0530

    PYTHON INTRO

---
+++ DAY-2/variable operations.py
@@ -0,0 +1,39 @@
+Python 3.14.6 (v3.14.6:c63aec69bd5, Jun 10 2026, 08:07:54) [Clang 21.0.0 (clang-2100.1.1.101)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> 
+=================================================================================== RESTART: /Users/rasooldudekula/Desktop/Python_courseworld/day2.py ==================================================================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> 
+================================================================================ RESTART: /Users/rasooldudekula/Desktop/Python_courseworld/DAY-2/day2.py ===============================================================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> 
+================================================================================ RESTART: /Users/rasooldudekula/Desktop/Python_courseworld/DAY-2/day2.py ===============================================================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c=120,240,360
+>>> a
+120
+>>> b
+240
+>>> c
+360
+>>> a,b=b,a
+>>> a
+240
+>>> b
+120
+>>> del c
+>>> c
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    c
+NameError: name 'c' is not defined
